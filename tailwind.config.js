5c1680bc0f0eda1e55a691133eb9cad438c759db
donrigoyeah <46565056+donrigoyeah@users.noreply.github.com>
2024-08-21 20:08:43 +0200

    added info in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  variants: {
+    extend: {
+      whiteSpace: ['hover'],
+      whitespace: ['hover'],
+    }
+  },
   theme: {
     fontFamily: {
       sans: ['Helvetica', 'Arial', 'sans-serif'],
@@ -15,10 +21,13 @@ module.exports = {
         secondary: '#B99B6B',
         third: '#698269',
         fourth: '#AA5656',
+        proGreen: '#31ab2a',
+        proBlack: '#694338'
       },
       boxShadow: {
         right: 'inset -20px 0px 10px -10px rgba(0,0,0,0.5)',
         left: 'inset 20px 0px 10px -10px rgba(0,0,0,0.5)',
+        inner: ' inset 0 10px 20px 0 rgba(0,0,0,0.5)'
       },
     },
   },
